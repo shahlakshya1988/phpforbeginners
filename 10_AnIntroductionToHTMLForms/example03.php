a36f26f7284b2512867e16ee24e50d019103fafc
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-11-07 08:33:58 +0530

    10, 3. Get Different Types of Data from the User Basic Input Types

---
+++ 10_AnIntroductionToHTMLForms/example03.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Different Types Of Controll</title>
+</head>
+<body>
+    <form action="">
+        <div>
+            <input type="text" name="username">
+        </div>
+        <div>
+            <input type="" name="firstname">
+        </div>
+        <div>
+            <input type="password" name="password">
+        </div>
+        <div>
+            <input type="email" name="email" id="email">
+        </div>
+        <div>
+            <input type="number" name="number" id="number">
+        </div>
+        <div>
+            <input type="date" name="date" id="date">
+        </div>
+        <div>
+            <input type="hidden" name="hiddeninput" id="hiddeninput">
+        </div>
+        <div>
+            <input type="color" name="" id="">
+        </div>
+        <div>
+            <input type="month" name="" id="">
+        </div>
+        <div class="">
+            <input type="range" name="" id="">
+        </div>
+        <div>
+            <input type="search">
+        </div>
+        <div>
+            <input type="date" name="" id="">
+        </div>
+        <div>
+            <input type="datetime-local" name="" id="">
+        </div>
+        <div>
+            <input type="week" name="" id="">
+        </div>
+        <div>
+            <input type="tel" name="" id="">
+        </div>
+        <div>
+            <input type="url" name="" id="">
+        </div>
+    </form>
+</body>
+</html>
