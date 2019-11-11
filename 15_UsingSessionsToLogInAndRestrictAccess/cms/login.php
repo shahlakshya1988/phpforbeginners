b24220776606e3e733e687cec1ef7fd6d2e1cfd7
Lakshya Shah <shahlakshya1988@gmail.com>
2019-11-11 18:35:35 +0530

    15,

--- 15_UsingSessionsToLogInAndRestrictAccess/cms/login.php
+++ 15_UsingSessionsToLogInAndRestrictAccess/cms/login.php
@@ -1,3 +1,46 @@
 <?php 
 session_start();
-$_SESSION["is_logged_in"] = true;
+require_once "includes/header.php"; 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	if(isset($_POST["login"])){
+		$username = trim($_POST["username"]);
+		$password = trim($_POST["password"]);
+		if($username == "dave" && $password == "123"){
+			die("Login Correct");
+			$_SESSION["is_logged_in"] = true;
+			header("Location: index.php");
+			die();
+ 		}else{
+			$errors[]="Login In-Correct";
+		}
+	}
+}
+?>
+<h2>Login</h2>
+<?php // var_dump($errors);?>
+<form action="" method="POST">
+	<?php
+	if(isset($errors) && !empty($errors)):
+		foreach($errors as $error){
+			?>
+				<p><?=$error;?></p>
+			<?php 
+		}
+	endif; 
+	?>
+	<div>
+		<label for="username">Username</label>
+		<input type="text" name="username" id="username">
+	</div>
+	<div>
+		<label for="password">Password</label>
+		<input type="password" name="password" id="password">
+	</div>
+	<div>
+		<button type="submit" name="login">Login</button>
+	</div>
+</form>
+<?php 
+require_once "includes/footer.php";
+?>
+
--- database/phpforbeginners_cms.sql
+++ database/phpforbeginners_cms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 11, 2019 at 05:11 AM
+-- Generation Time: Nov 11, 2019 at 01:04 PM
 -- Server version: 5.7.26-log
 -- PHP Version: 7.2.18
 
@@ -29,23 +29,18 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS `article`;
-CREATE TABLE IF NOT EXISTS `article` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `article` (
+  `id` int(255) NOT NULL,
   `title` varchar(128) NOT NULL,
   `content` text NOT NULL,
-  `published_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `title` (`title`)
-) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+  `published_at` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `article`
 --
 
 INSERT INTO `article` (`id`, `title`, `content`, `published_at`) VALUES
-(1, 'First Article', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec eleifend massa. Phasellus orci magna, mattis imperdiet eleifend facilisis, lacinia ac sem. Mauris venenatis rutrum diam, in eleifend odio consequat dictum. Proin et pharetra nunc. Pellentesque mattis metus nec ex posuere faucibus. Phasellus porttitor dolor sapien, quis sodales turpis varius in. Proin tincidunt lorem diam, ac euismod erat sagittis sed. Nulla efficitur vestibulum dolor, sit amet molestie leo porttitor id.</p>\r\n\r\n<p>Maecenas dapibus vel risus nec euismod. Ut leo mauris, luctus sit amet maximus sit amet, hendrerit faucibus mi. Fusce porttitor congue risus. Nam neque mauris, sagittis vitae finibus rhoncus, venenatis in justo. Sed suscipit feugiat augue ac faucibus. Nam arcu velit, efficitur nec massa vitae, dapibus pharetra nunc. Etiam tempor nunc vitae dapibus finibus. In dapibus magna eu tellus vestibulum convallis. Nunc ligula mauris, sollicitudin tristique bibendum at, placerat hendrerit tortor.</p>\r\n\r\n<p>Ut quis felis congue, luctus magna vitae, semper metus. Vestibulum vel nibh tristique, dignissim erat eu, rhoncus diam. Pellentesque mattis blandit tempus. Sed a sollicitudin velit. Pellentesque egestas mi sit amet finibus gravida. Phasellus nibh nulla, auctor ut venenatis at, malesuada quis justo. Vestibulum fermentum sapien non ligula dignissim, id pellentesque metus placerat. Integer et imperdiet lorem. Morbi ut cursus sapien. Mauris dapibus urna non lacus pretium lobortis. Quisque sapien purus, tempor ultrices justo nec, viverra pharetra ipsum. Curabitur eget congue odio. Suspendisse sit amet ultrices dolor, vitae auctor nunc. Integer tortor purus, mollis quis convallis eget, faucibus sit amet nibh. Pellentesque risus turpis, sodales id eros eu, vehicula venenatis justo.</p>', '2019-09-12 00:00:00'),
-(2, 'Second Article', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec eleifend massa. Phasellus orci magna, mattis imperdiet eleifend facilisis, lacinia ac sem. Mauris venenatis rutrum diam, in eleifend odio consequat dictum. Proin et pharetra nunc. Pellentesque mattis metus nec ex posuere faucibus. Phasellus porttitor dolor sapien, quis sodales turpis varius in. Proin tincidunt lorem diam, ac euismod erat sagittis sed. Nulla efficitur vestibulum dolor, sit amet molestie leo porttitor id.</p>\r\n\r\n<p>Maecenas dapibus vel risus nec euismod. Ut leo mauris, luctus sit amet maximus sit amet, hendrerit faucibus mi. Fusce porttitor congue risus. Nam neque mauris, sagittis vitae finibus rhoncus, venenatis in justo. Sed suscipit feugiat augue ac faucibus. Nam arcu velit, efficitur nec massa vitae, dapibus pharetra nunc. Etiam tempor nunc vitae dapibus finibus. In dapibus magna eu tellus vestibulum convallis. Nunc ligula mauris, sollicitudin tristique bibendum at, placerat hendrerit tortor.</p>\r\n\r\n<p>Ut quis felis congue, luctus magna vitae, semper metus. Vestibulum vel nibh tristique, dignissim erat eu, rhoncus diam. Pellentesque mattis blandit tempus. Sed a sollicitudin velit. Pellentesque egestas mi sit amet finibus gravida. Phasellus nibh nulla, auctor ut venenatis at, malesuada quis justo. Vestibulum fermentum sapien non ligula dignissim, id pellentesque metus placerat. Integer et imperdiet lorem. Morbi ut cursus sapien. Mauris dapibus urna non lacus pretium lobortis. Quisque sapien purus, tempor ultrices justo nec, viverra pharetra ipsum. Curabitur eget congue odio. Suspendisse sit amet ultrices dolor, vitae auctor nunc. Integer tortor purus, mollis quis convallis eget, faucibus sit amet nibh. Pellentesque risus turpis, sodales id eros eu, vehicula venenatis justo.</p>', '2019-11-01 00:00:00'),
-(3, 'Third Article', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec eleifend massa. Phasellus orci magna, mattis imperdiet eleifend facilisis, lacinia ac sem. Mauris venenatis rutrum diam, in eleifend odio consequat dictum. Proin et pharetra nunc. Pellentesque mattis metus nec ex posuere faucibus. Phasellus porttitor dolor sapien, quis sodales turpis varius in. Proin tincidunt lorem diam, ac euismod erat sagittis sed. Nulla efficitur vestibulum dolor, sit amet molestie leo porttitor id.</p>\r\n\r\n<p>Maecenas dapibus vel risus nec euismod. Ut leo mauris, luctus sit amet maximus sit amet, hendrerit faucibus mi. Fusce porttitor congue risus. Nam neque mauris, sagittis vitae finibus rhoncus, venenatis in justo. Sed suscipit feugiat augue ac faucibus. Nam arcu velit, efficitur nec massa vitae, dapibus pharetra nunc. Etiam tempor nunc vitae dapibus finibus. In dapibus magna eu tellus vestibulum convallis. Nunc ligula mauris, sollicitudin tristique bibendum at, placerat hendrerit tortor.</p>\r\n\r\n<p>Ut quis felis congue, luctus magna vitae, semper metus. Vestibulum vel nibh tristique, dignissim erat eu, rhoncus diam. Pellentesque mattis blandit tempus. Sed a sollicitudin velit. Pellentesque egestas mi sit amet finibus gravida. Phasellus nibh nulla, auctor ut venenatis at, malesuada quis justo. Vestibulum fermentum sapien non ligula dignissim, id pellentesque metus placerat. Integer et imperdiet lorem. Morbi ut cursus sapien. Mauris dapibus urna non lacus pretium lobortis. Quisque sapien purus, tempor ultrices justo nec, viverra pharetra ipsum. Curabitur eget congue odio. Suspendisse sit amet ultrices dolor, vitae auctor nunc. Integer tortor purus, mollis quis convallis eget, faucibus sit amet nibh. Pellentesque risus turpis, sodales id eros eu, vehicula venenatis justo.</p>', '2019-10-10 00:00:00'),
 (4, 'Fourth Article', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec eleifend massa. Phasellus orci magna, mattis imperdiet eleifend facilisis, lacinia ac sem. Mauris venenatis rutrum diam, in eleifend odio consequat dictum. Proin et pharetra nunc. Pellentesque mattis metus nec ex posuere faucibus. Phasellus porttitor dolor sapien, quis sodales turpis varius in. Proin tincidunt lorem diam, ac euismod erat sagittis sed. Nulla efficitur vestibulum dolor, sit amet molestie leo porttitor id.</p>\r\n\r\n<p>Maecenas dapibus vel risus nec euismod. Ut leo mauris, luctus sit amet maximus sit amet, hendrerit faucibus mi. Fusce porttitor congue risus. Nam neque mauris, sagittis vitae finibus rhoncus, venenatis in justo. Sed suscipit feugiat augue ac faucibus. Nam arcu velit, efficitur nec massa vitae, dapibus pharetra nunc. Etiam tempor nunc vitae dapibus finibus. In dapibus magna eu tellus vestibulum convallis. Nunc ligula mauris, sollicitudin tristique bibendum at, placerat hendrerit tortor.</p>\r\n\r\n<p>Ut quis felis congue, luctus magna vitae, semper metus. Vestibulum vel nibh tristique, dignissim erat eu, rhoncus diam. Pellentesque mattis blandit tempus. Sed a sollicitudin velit. Pellentesque egestas mi sit amet finibus gravida. Phasellus nibh nulla, auctor ut venenatis at, malesuada quis justo. Vestibulum fermentum sapien non ligula dignissim, id pellentesque metus placerat. Integer et imperdiet lorem. Morbi ut cursus sapien. Mauris dapibus urna non lacus pretium lobortis. Quisque sapien purus, tempor ultrices justo nec, viverra pharetra ipsum. Curabitur eget congue odio. Suspendisse sit amet ultrices dolor, vitae auctor nunc. Integer tortor purus, mollis quis convallis eget, faucibus sit amet nibh. Pellentesque risus turpis, sodales id eros eu, vehicula venenatis justo.</p>', '2019-10-22 00:00:00'),
 (5, 'Fifth Article', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec eleifend massa. Phasellus orci magna, mattis imperdiet eleifend facilisis, lacinia ac sem. Mauris venenatis rutrum diam, in eleifend odio consequat dictum. Proin et pharetra nunc. Pellentesque mattis metus nec ex posuere faucibus. Phasellus porttitor dolor sapien, quis sodales turpis varius in. Proin tincidunt lorem diam, ac euismod erat sagittis sed. Nulla efficitur vestibulum dolor, sit amet molestie leo porttitor id.</p>\r\n\r\n<p>Maecenas dapibus vel risus nec euismod. Ut leo mauris, luctus sit amet maximus sit amet, hendrerit faucibus mi. Fusce porttitor congue risus. Nam neque mauris, sagittis vitae finibus rhoncus, venenatis in justo. Sed suscipit feugiat augue ac faucibus. Nam arcu velit, efficitur nec massa vitae, dapibus pharetra nunc. Etiam tempor nunc vitae dapibus finibus. In dapibus magna eu tellus vestibulum convallis. Nunc ligula mauris, sollicitudin tristique bibendum at, placerat hendrerit tortor.</p>\r\n\r\n<p>Ut quis felis congue, luctus magna vitae, semper metus. Vestibulum vel nibh tristique, dignissim erat eu, rhoncus diam. Pellentesque mattis blandit tempus. Sed a sollicitudin velit. Pellentesque egestas mi sit amet finibus gravida. Phasellus nibh nulla, auctor ut venenatis at, malesuada quis justo. Vestibulum fermentum sapien non ligula dignissim, id pellentesque metus placerat. Integer et imperdiet lorem. Morbi ut cursus sapien. Mauris dapibus urna non lacus pretium lobortis. Quisque sapien purus, tempor ultrices justo nec, viverra pharetra ipsum. Curabitur eget congue odio. Suspendisse sit amet ultrices dolor, vitae auctor nunc. Integer tortor purus, mollis quis convallis eget, faucibus sit amet nibh. Pellentesque risus turpis, sodales id eros eu, vehicula venenatis justo.</p>', '2019-11-02 10:14:47'),
 (6, 'hello', 'hello world', '2019-11-09 04:01:00'),
@@ -58,9 +53,29 @@ INSERT INTO `article` (`id`, `title`, `content`, `published_at`) VALUES
 (13, 'hello\' \"', 'asdfasdf', '2019-11-22 14:00:00'),
 (14, 'ddd', 'asdfasdfsadf', '2018-01-05 01:00:00'),
 (15, '<script>alert(\\\\\\\"Hello World\\\\\\\");</script>', 'conent', '2019-11-22 13:00:00'),
-(16, 'ddddawesrwe', '', NULL),
 (17, 'asdf', '', '0000-00-00 00:00:00'),
 (18, 'asdf', '', '0000-00-00 00:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `title` (`title`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `article`
+--
+ALTER TABLE `article`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
