5166b4b39328e63a81235a128ecef8cb1232828c
Lakshya Shah <shahlakshya1988@gmail.com>
2019-11-12 11:36:18 +0530

    16,2. Object Attributes Adding Properties to a Class

--- 16_AnIntroductionToObjectOrientedPHP/Item.php
+++ 16_AnIntroductionToObjectOrientedPHP/Item.php
@@ -1,4 +1,9 @@
 <?php 
 class Item{
-
+	/**
+	 * by default all the properties are set to null 
+	 * if no value is passed
+	 */
+	public $name; // properties are variables inside of class defination
+	public $description; //properties are variables assosiated with objects individually
 }
--- 16_AnIntroductionToObjectOrientedPHP/example01.php
+++ 16_AnIntroductionToObjectOrientedPHP/example01.php
@@ -1,6 +1,15 @@
 <?php 
 require "Item.php";
 $myItem = new Item();
+$myItem->name="Item 1";
+$myItem->description="Item 1 Description";
+$myItem->notedecelared="Not Decelared In class";
 var_dump($myItem);
+var_dump($myItem->name);
+var_dump($myItem->description);
+var_dump($myItem->notedecelared);
+echo "<br>******************<br>";
 $myItem2 = new Item();
 var_dump($myItem2);
+var_dump($myItem2->name);
+var_dump($myItem2->description);
