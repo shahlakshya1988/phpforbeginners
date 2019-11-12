03a528458202b988c7f5e09391f00b3867ee23ab
Lakshya Shah <shahlakshya1988@gmail.com>
2019-11-12 14:41:33 +0530

    16, 3. Object Functions Adding Methods to a Class

--- 16_AnIntroductionToObjectOrientedPHP/Item.php
+++ 16_AnIntroductionToObjectOrientedPHP/Item.php
@@ -6,4 +6,11 @@ class Item{
 	 */
 	public $name; // properties are variables inside of class defination
 	public $description; //properties are variables assosiated with objects individually
+
+	public function sayHello(){
+		echo "Hello <br>";
+	}
+	public function getName(){
+		return $this->name;
+	}
 }
