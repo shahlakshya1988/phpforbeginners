5921c03c5c49888d813405c3d9d52d498dcb8d6a
Lakshya Shah <shahlakshya1988@gmail.com>
2019-11-28 11:19:00 +0530

    18, 3. Authenticate the User with Data from the Database

--- cms/classes/User.php
+++ cms/classes/User.php
@@ -3,7 +3,22 @@ class User{
 	public $id;
 	public $username;
 	public $password;
-	public static function authenticate($username,$password){
-		return ($_POST['username'] == 'dave' && $_POST['password'] == '123');
+	public static function authenticate($conn,$username,$password){
+		$sql="SELECT * FROM `user` where `username`=:username";
+		$stmt = $conn->prepare($sql);
+		$stmt->bindParam(":username",$username,PDO::PARAM_STR);
+		$stmt->setFetchMode(PDO::FETCH_CLASS,"User");
+		$stmt->execute();
+		$user = $stmt->fetch();
+		if($user){
+			if($user->password == $password){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
+
 	}
 }
--- cms/login.php
+++ cms/login.php
@@ -2,12 +2,15 @@
 
 require 'includes/url.php';
 require 'classes/User.php';
+require "classes/Database.php";
 
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if (User::authenticate($_POST["username"],$_POST["password"])) {
+    $db = new Database();
+    $conn = $db->getConn();
+    if (User::authenticate($conn,$_POST["username"],$_POST["password"])) {
 
         session_regenerate_id(true);
 
