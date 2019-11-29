03d01873dd08ef5011a7d50a4c70cf228ef69fcd
Lakshya Shah <shahlakshya1988@gmail.com>
2019-11-29 11:03:45 +0530

    20, 2. Create an Admin Index and Load Classes Relative to the site root Directory

--- cms/includes/init.php
+++ cms/includes/init.php
@@ -1,6 +1,7 @@
 <?php 
+//var_dump(dirname(__DIR__));
 spl_autoload_register(function($classname){
-    $file_path = "classes/{$classname}.php";
+    $file_path = dirname(__DIR__)."/classes/{$classname}.php";
     if(file_exists($file_path)){
         require_once $file_path;
     }
