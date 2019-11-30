6751728009128595bfae5af347c3d75db8768acb
Lakshya Shah <shahlakshya1988@gmail.com>
2019-11-30 11:38:21 +0530

    21, 5. Validate the Page Number using the filter_var Function

--- cms/classes/Paginator.php
+++ cms/classes/Paginator.php
@@ -3,6 +3,9 @@ class Paginator{
   public $limit;
   public $offset;
   public function __construct($page,$records_per_page){
+    //var_dump($page);
+    $page = filter_var($page,FILTER_VALIDATE_INT);
+    //var_dump($page);
     if($page<=1){
       $page=1;
     }
