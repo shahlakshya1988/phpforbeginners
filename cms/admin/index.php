590bfb5540157e48092df60ae3bd6b2ec86e96a6
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-11-30 07:23:06 +0530

    20, 5. Add Sitewide Navigation Links

--- cms/admin/index.php
+++ cms/admin/index.php
@@ -10,19 +10,9 @@ $articles = Article::getAll($conn);
 ?>
 <?php require '../includes/header.php'; ?>
 
-<?php if (Auth::isLoggedIn()) : ?>
-    
-    <p>You are logged in. <a href="logout.php">Log out</a></p>
-    <p><a href="new-article.php">New article</a></p>
-
-<?php else : ?>
-    
-    <p>You are not logged in. <a href="login.php">Log in</a></p>
-
-<?php endif; ?>
 
 <h2>Administration</h2>
-
+    <p><a href="new-article.php">New article</a></p>
 <?php if (empty($articles)) : ?>
     <p>No articles found.</p>
 <?php else : ?>
@@ -38,8 +28,8 @@ $articles = Article::getAll($conn);
                 <td>
                     <a href="article.php?id=<?= $article['id']; ?>"><?= htmlspecialchars($article['title']); ?></a>
                  </td>
-             </tr>       
-              
+             </tr>
+
         <?php endforeach; ?>
     </tbody>
 </table>
--- cms/includes/header.php
+++ cms/includes/header.php
@@ -9,5 +9,16 @@
     <header>
         <h1>My blog</h1>
     </header>
+    <nav>
+        <ul>
+            <li> <a href="/index.php">Home</a> </li>
+            <?php if(Auth::isLoggedIn()){ ?>
+                <li> <a href="/admin/">Admin</a> </li>
+                <li><a href="/logout.php">Log Out</a> </li>
+            <?php }else{ ?>
+                <li><a href="/login.php">Login</a> </li>
 
+            <?php } ?>
+        </ul>
+    </nav>
     <main>
